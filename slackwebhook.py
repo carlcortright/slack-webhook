c49dfc5581638616bc5fcdaf453c3d6ec9a13946
Carl Cortright <ckcortright@gmail.com>
2016-12-20 12:35:03 -0600

    Tests complete

--- slackwebhook.py
+++ slackwebhook.py
@@ -17,41 +17,40 @@ class slackwebhook:
         self.webhook_url = webhook_url
 
     #
-    # Post update to slack
+    # Post a simple update to slack
     #
     def post(self, text):
         payload = { "text" : text}
-        print(payload)
-        response = requests.post(self.webhook_url, json=payload)
-        if(response.status_code != 200):
-            print("ERROR: the url %s returned a %d response code." % (self.webhook_url, response.status_code))
-        return response.status_code
+        status = self.__post_payload(payload)
+        return status
 
     #
     # Posts a richly formatted post to slack
     #
-    def rich_format_post(self, text=None, fallback=None, pretext=None, color=None, title=None, value=None, short=None):
+    def rich_format_post(self, fallback=None, text=None, pretext=None, color=None, title=None, value=None, short=None):
         # Create a richly formatted payload
         payload = {
         "attachments":[
-                "fallback": fallback,
-                "text": text,
-                "pretext": pretext,
-                "color": color,
-                "fields":[
-                    {
-                        "title": title,
-                        "value": value,
-                        "short", short
-                    }
-                ]
+                {
+                    "fallback": fallback,
+                    "text": text,
+                    "pretext": pretext,
+                    "color": color,
+                    "fields":[
+                        {
+                            "title": title,
+                            "value": value,
+                            "short": short
+                        }
+                    ]
+                }
             ]
         }
         status = self.__post_payload(payload)
         return status
 
     #
-    # Post the json payload to slack
+    # Post a json payload to slack webhook url
     #
     def __post_payload(self, payload):
         response = requests.post(self.webhook_url, json=payload)
--- tests.py
+++ tests.py
@@ -7,3 +7,22 @@
 ################################################################################
 import unittest
 from slackwebhook import slackwebhook
+
+
+class TestSlackWebhook(unittest.TestCase):
+    webhook = slackwebhook("https://hooks.slack.com/services/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")
+
+    def test_simple_post(self):
+        self.assertEqual(self.webhook.post("Testing simple text post"), 200)
+
+    def test_richformat_post(self):
+        self.assertEqual(self.webhook.rich_format_post(text="test with only fallback"), 200)
+        self.assertEqual(self.webhook.rich_format_post(fallback="test with fallback", text="test with fallbacks"), 200)
+        self.assertEqual(self.webhook.rich_format_post(fallback="Full rich format test", text="Full rich format test", pretext="Checkout this cool thing:", title="Cool Stuff!", value="Here's some more info", short=False, color="#000000"), 200)
+        self.assertEqual(self.webhook.rich_format_post(fallback="Full rich format test", text="Full rich format test", pretext="Checkout this cool thing:", title="Cool Stuff!", value="Short is true on this one", short=True, color="#00FF00"), 200)
+
+    def test_link(self):
+        self.assertEqual(self.webhook.post("This is to test links: <https://www.google.com|Link to Google>"), 200)
+    
+if __name__ == '__main__':
+    unittest.main()
