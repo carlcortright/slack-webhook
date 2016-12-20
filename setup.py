3faf76646826817f685f6b031c22696538311678
Carl Cortright <ckcortright@gmail.com>
2016-12-20 14:07:57 -0600

    v0.2 uploaded to pypi

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ from distutils.core import setup
 setup(
   name = 'slackwebhook',
   packages = ['slackwebhook'],
-  version = '0.1',
+  version = '0.2',
   description = 'Easy interface for posting to a slack webhook',
   author = 'Carl Cortright',
   author_email = 'ckcortright@gmail.com',
   url = 'https://github.com/carlcortright/slack-webhook',
-  download_url = 'https://github.com/carlcortright/slack-webhook/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/carlcortright/slack-webhook/tarball/0.2', # I'll explain this in a second
   keywords = ['slack', 'webhook', 'bot', 'easy'],
   classifiers = [],
   install_requires=[
